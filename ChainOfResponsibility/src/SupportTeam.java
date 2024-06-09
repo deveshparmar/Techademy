d789d569ae7f4202d1cbed864c4e1cfff1de7404
Devesh Parmar <81907545+deveshparmar@users.noreply.github.com>
2024-06-09 14:38:03 +0530

    Delete ChainOfResponsibility/src/SupportTeam.java

--- ChainOfResponsibility/src/SupportTeam.java
+++
@@ -1,9 +0,0 @@
-public abstract class SupportTeam {
-    protected SupportTeam successor;
-
-    public void setSuccessor(SupportTeam successor) {
-        this.successor = successor;
-    }
-
-    public abstract void handleRequest(Ticket ticket);
-}
