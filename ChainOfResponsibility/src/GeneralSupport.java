65d81150a4fca787b9d1f2408d60eea46b5a9f84
Devesh Parmar <81907545+deveshparmar@users.noreply.github.com>
2024-06-09 14:37:51 +0530

    Delete ChainOfResponsibility/src/GeneralSupport.java

--- ChainOfResponsibility/src/GeneralSupport.java
+++
@@ -1,6 +0,0 @@
-public class GeneralSupport extends SupportTeam{
-    @Override
-    public void handleRequest(Ticket ticket) {
-        System.out.println("General support team handling issue for " + ticket.getCustomerName());
-    }
-}
